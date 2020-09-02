590dc3001d24fb583014c44e05fff543c44d4396
Leandro Marceddu <leandromarceddu@gmail.com>
2020-09-02 13:12:53 +0200

    Add project files.

---
+++ DSHModbus/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace DSHModbus
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbCOM = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBaud = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSlaveID = new System.Windows.Forms.TextBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            this.lsbDebug = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtSlaveID);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtBaud);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cmbCOM);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Instellingen";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM-poort";
+            // 
+            // cmbCOM
+            // 
+            this.cmbCOM.FormattingEnabled = true;
+            this.cmbCOM.Location = new System.Drawing.Point(70, 13);
+            this.cmbCOM.Name = "cmbCOM";
+            this.cmbCOM.Size = new System.Drawing.Size(121, 21);
+            this.cmbCOM.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Baud";
+            // 
+            // txtBaud
+            // 
+            this.txtBaud.Location = new System.Drawing.Point(70, 41);
+            this.txtBaud.Name = "txtBaud";
+            this.txtBaud.Size = new System.Drawing.Size(121, 20);
+            this.txtBaud.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Slave ID";
+            // 
+            // txtSlaveID
+            // 
+            this.txtSlaveID.Location = new System.Drawing.Point(70, 67);
+            this.txtSlaveID.Name = "txtSlaveID";
+            this.txtSlaveID.Size = new System.Drawing.Size(121, 20);
+            this.txtSlaveID.TabIndex = 5;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(218, 23);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(84, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Ververs COM";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(218, 51);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(84, 23);
+            this.btnTest.TabIndex = 2;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            // 
+            // lsbDebug
+            // 
+            this.lsbDebug.FormattingEnabled = true;
+            this.lsbDebug.Items.AddRange(new object[] {
+            "femzlfzeeµ",
+            "fzeùfzef",
+            "ezfz",
+            "zzz",
+            "z",
+            "zz",
+            "z",
+            "zz",
+            "z",
+            "z",
+            "z",
+            "z",
+            "z",
+            "z",
+            "z"});
+            this.lsbDebug.Location = new System.Drawing.Point(12, 118);
+            this.lsbDebug.Name = "lsbDebug";
+            this.lsbDebug.Size = new System.Drawing.Size(290, 173);
+            this.lsbDebug.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 304);
+            this.Controls.Add(this.lsbDebug);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmMain";
+            this.Text = "DSH Modbus Test";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSlaveID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBaud;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbCOM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnTest;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.IO.Ports.SerialPort serialPort2;
+        private System.Windows.Forms.ListBox lsbDebug;
+    }
+}
+
